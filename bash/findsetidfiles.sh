cb6fccc8ab020e17228f9bbafa3f638958a3a9a4
JerryPatelAdi <pateler97@gmail.com>
2019-09-13 11:24:50 -0700

    Done

---
+++ bash/findsetidfiles.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+#SetUid Files:::::
+daemon -rwsr-sr-x /usr/bin/at
+root -rwsr-sr-x /usr/lib/snapd/snap-confine
+root -rwsr-xr-x /bin/fusermount
+root -rwsr-xr-x /bin/mount
+root -rwsr-xr-x /bin/ping
+root -rwsr-xr-x /bin/su
