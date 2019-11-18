02c4e697b1f61e261dc726f9a3235528a0879f8f
JerryPatelAdi <54998302+JerryPatelAdi@users.noreply.github.com>
2019-11-18 16:45:42 -0500

    D

--- Powershell/Disk_Report.ps1
+++ Powershell/Disk_Report.ps1
@@ -1,1 +1,1 @@
-﻿get-wmiobject -class win32_logicaldisk | Where-Object -Property Size -ne "0" | Format-Table DeviceID,{$_.Size/1mb},{$_.FreeSpace/1mb},ProviderName
+﻿get-wmiobject -class win32_logicaldisk | Where-Object -Property Size -ne "0" | Format-Table DeviceID,@{n="Size(MB)"; e={$_.Size/1mb -as [int]}},@{n="Free(MB)"; e={$_.freespace/1mb -as [int]}},ProviderName
